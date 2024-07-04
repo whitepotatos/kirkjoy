ab4a1a7a4409c87c59e7c08ab1824d594a07e150
whitepotatos <kirkck477@gmail.com>
2024-07-04 22:47:09 +0800

    first commit

---
+++ src/main/java/org/potato/kirkjoy/KirkjoyApplication.java
@@ -0,0 +1,13 @@
+package org.potato.kirkjoy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KirkjoyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KirkjoyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/potato/kirkjoy/KirkjoyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.potato.kirkjoy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KirkjoyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
